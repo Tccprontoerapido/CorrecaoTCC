c47f029d838df2487d46f86d1d03bcd8b9f1666b
NelioSites <neliolima598@gmail.com>
2025-03-16 11:25:53 -0300

    Delete componentes directory
    exclusão após incorporação em outro diretorio

--- componentes/verificador-formatacao.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.replace('editor');
-
-function verificarFormatacao() {
-    const texto = CKEDITOR.instances.editor.getData();
-    let erros = [];
-    let textoFormatado = texto;
-
-    // Outras verificações de formatação aqui
-}
